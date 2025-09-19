bc22e15f722cb4fcb822c6b42e2a14f6d9f7864d
leechangsu <changw2675@gmail.com>
2025-09-19 09:27:01 +0900

    remove key

--- projects/pj4(search_engine)/web_searching.py
+++ projects/pj4(search_engine)/web_searching.py
@@ -13,7 +13,7 @@ class search_engine:
         if not query:
             return
         tavily_search_engine = TavilySearchAPIWrapper(
-            tavily_api_key="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"
+            tavily_api_key=""
         )
         return[r["url"] for r in tavily_search_engine.results(query=query,max_results=max_results,search_depth="advanced")]
 
@@ -21,7 +21,7 @@ class search_engine:
         if not query:
             return
         serp_search_engine = SerpAPIWrapper(
-            serpapi_api_key="8d73dfbfe66cb9c11254c04d6aef3932a6f501fdea81867fbea45ed6e0e8eb28"
+            serpapi_api_key=""
         )
         answer_api = serp_search_engine.results(query=query)
         res = [copy.deepcopy(answer_api['organic_results'])]
@@ -31,7 +31,7 @@ class search_engine:
         if not query:
             return
         serper_search_engine = GoogleSerperAPIWrapper(
-            serper_api_key="af1f4df11e53cf75f5decf12d6e5987f2ec35a31"
+            serper_api_key=""
         )
         res = [serper_search_engine.results(query=query)['organic']]
         return [j["link"] for i in res for j in i if j["position"] <= int(max_results)]
